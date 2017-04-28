6e5a700739f380d1edb77c7cbfe61a492dddd9a9
Mathias Karner <karner@iiasa.ac.at>
2017-04-28 13:24:12 +0200

    Localization feature

--- CimmytApp/CimmytApp/App.xaml.cs
+++ CimmytApp/CimmytApp/App.xaml.cs
@@ -1,13 +1,28 @@
-﻿using CimmytApp.Views;
-using Prism.Unity;
-using Xamarin.Forms;
-
-namespace CimmytApp
+﻿namespace CimmytApp
 {
+    using System.Reflection;
+    using Prism.Unity;
+    using Xamarin.Forms;
+
+    using Localization;
+    using Views;
+
     public partial class App : PrismApplication
     {
         public App(IPlatformInitializer initializer = null) : base(initializer)
         {
+            System.Diagnostics.Debug.WriteLine("====== resource debug info =========");
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+                System.Diagnostics.Debug.WriteLine("found resource: " + res);
+            System.Diagnostics.Debug.WriteLine("====================================");
+
+            if (Device.OS == TargetPlatform.iOS || Device.OS == TargetPlatform.Android)
+            {
+                var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+                Resx.AppResources.Culture = ci;
+                DependencyService.Get<ILocalize>().SetLocale(ci);
+            }
         }
 
         protected override void OnInitialized()
---
+++ CimmytApp/CimmytApp/Localization/TranslateExtension.cs
@@ -0,0 +1,42 @@
+﻿namespace CimmytApp.Localization
+{
+    using System;
+    using System.Globalization;
+    using System.Reflection;
+    using System.Resources;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        private readonly CultureInfo _ci;
+        private const string ResourceId = "CimmytApp.Resx.AppResources";
+
+        public string Text { get; set; }
+
+        public TranslateExtension()
+        {
+            if (Device.OS == TargetPlatform.iOS || Device.OS == TargetPlatform.Android)
+            {
+                _ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            }
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null) return "";
+            var resourceManager = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+            var translation = resourceManager.GetString(Text, _ci);
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException($"Key '{Text}' was not found in resources '{ResourceId}' for culture '{_ci.Name}'.", "Text");
+#else
+				translation = Text; // HACK: returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+    }
+}
