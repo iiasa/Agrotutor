a2c884a59073d5bdf818c2a0aaccafc72dee9de1
Moamen Saad <saad@iiasa.ac.at>
2017-05-02 15:34:11 +0200

    Create Map Component

---
+++ Helper.Map/Views/GenericMap.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Helper.Map.Views
+{
+    public partial class GenericMap : ContentPage
+    {
+        public GenericMap()
+        {
+            InitializeComponent();
+        }
+    }
+}
