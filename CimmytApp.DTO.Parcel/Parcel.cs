1b2967688dc9db7aeac8163860c51b61284c404a
Mathias Karner <karner@iiasa.ac.at>
2017-07-26 20:34:08 +0200

    Added Propertychanged notifications

--- CimmytApp.DTO.Parcel/Parcel.cs
+++ CimmytApp.DTO.Parcel/Parcel.cs
@@ -17,47 +17,189 @@
     {
         private static int geoWikiDatasetGroupId = 1;
 
+        private string _agriculturalCycle;
+        private string _crop;
+        private string _cultivar;
+        private string _estimatedParcelArea;
+        private GeoPosition _geoPosition;
         private int _id;
-        public int ID { get { return _id; } set { _id = value; OnPropertyChanged("ID"); } }
-        public string ParcelName { get; set; }
-        public string Crop { get; set; }
-        public string Cultivar { get; set; }
-        public string AgriculturalCycle { get; set; }
-		public string Year { get; set; }
-		public string EstimatedParcelArea { get; set; }
-        public string EstimatedParcelAreaWithUnit { get { return EstimatedParcelArea + " ha"; } }
-        public string ProducerName { get; set; }
-        public List<string> TechnologiesUsed { get; set; }
-        public string OtherTechnologies { get; set; }
+        private string _irrigation;
+        private string _otherTechnologies;
+        private string _parcelName;
+        private string _performance;
+        private List<PesticideApplication> _pesticidesApplied;
+        private DateTime _plantingDate;
+        private string _producerName;
+        private List<string> _technologiesUsed;
+        private string _year;
 
-		public string Performance { get; set; }
-        public string PerformanceWithUnit { get { return Performance + " tons/ha"; } }
-        public string Irrigation { get; set; }
-        public DateTime PlantingDate { get; set; }
-        public List<PesticideApplication> PesticidesApplied { get; set; }
-
-        public GeoPosition GeoPosition { get; set; }
-
-        public string TechnologiesScreenList {
-            get { return String.Join("\r\n", TechnologiesUsed.ToArray()); }
-        }
-
-        public int CompletedPercentage => 10;
-        public string OverviewString => $"{Crop}\r\n({CompletedPercentage} % complete)";
-        public string IconSource => $"corn.png";
-
-        public Parcel(){
+        public Parcel()
+        {
             PlantingDate = DateTime.Today;
         }
 
-        public DataTemplate GetOverviewDataTemplate()
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string AgriculturalCycle
         {
-            return null;
+            get { return _agriculturalCycle; }
+            set
+            {
+                _agriculturalCycle = value;
+                OnPropertyChanged("AgriculturalCycle");
+            }
         }
 
-        public async void Submit()
+        public int CompletedPercentage => 10;
+
+        public string Crop
         {
-            await Storage.StoreDatasetAsync(this, -1, 16, 1, geoWikiDatasetGroupId);
+            get { return _crop; }
+            set
+            {
+                _crop = value;
+                OnPropertyChanged("Crop");
+            }
+        }
+
+        public string Cultivar
+        {
+            get { return _cultivar; }
+            set
+            {
+                _cultivar = value;
+                OnPropertyChanged("Cultivar");
+            }
+        }
+
+        public string EstimatedParcelArea
+        {
+            get { return _estimatedParcelArea; }
+            set
+            {
+                _estimatedParcelArea = value;
+                OnPropertyChanged("EstimatedParcelArea");
+            }
+        }
+
+        public string EstimatedParcelAreaWithUnit => EstimatedParcelArea + " ha";
+
+        public GeoPosition GeoPosition
+        {
+            get { return _geoPosition; }
+            set
+            {
+                _geoPosition = value;
+                OnPropertyChanged("GeoPosition");
+            }
+        }
+
+        public string IconSource => $"corn.png";
+
+        public int ID
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged("ID");
+            }
+        }
+
+        public string Irrigation
+        {
+            get { return _irrigation; }
+            set
+            {
+                _irrigation = value;
+                OnPropertyChanged("Irrigation");
+            }
+        }
+
+        public string OtherTechnologies
+        {
+            get { return _otherTechnologies; }
+            set
+            {
+                _otherTechnologies = value;
+                OnPropertyChanged("OtherTechnologies");
+            }
+        }
+
+        public string OverviewString => $"{Crop}\r\n({CompletedPercentage} % complete)";
+
+        public string ParcelName
+        {
+            get { return _parcelName; }
+            set
+            {
+                _parcelName = value;
+                OnPropertyChanged("ParcelNames");
+            }
+        }
+
+        public string Performance
+        {
+            get { return _performance; }
+            set
+            {
+                _performance = value;
+                OnPropertyChanged("Performance");
+            }
+        }
+
+        public string PerformanceWithUnit => Performance + " tons/ha";
+
+        public List<PesticideApplication> PesticidesApplied
+        {
+            get { return _pesticidesApplied; }
+            set
+            {
+                _pesticidesApplied = value;
+                OnPropertyChanged("PesticidesApplied");
+            }
+        }
+
+        public DateTime PlantingDate
+        {
+            get { return _plantingDate; }
+            set
+            {
+                _plantingDate = value;
+                OnPropertyChanged("PlantingDate");
+            }
+        }
+
+        public string ProducerName
+        {
+            get { return _producerName; }
+            set
+            {
+                _producerName = value;
+                OnPropertyChanged("ProducerName");
+            }
+        }
+
+        public string TechnologiesScreenList => string.Join("\r\n", TechnologiesUsed.ToArray());
+
+        public List<string> TechnologiesUsed
+        {
+            get { return _technologiesUsed; }
+            set
+            {
+                _technologiesUsed = value;
+                OnPropertyChanged("TechnologiesUsed");
+            }
+        }
+
+        public string Year
+        {
+            get { return _year; }
+            set
+            {
+                _year = value;
+                OnPropertyChanged("Year");
+            }
         }
 
         public static async Task<List<Parcel>> LoadParcelsFromServer()
@@ -65,21 +207,26 @@
             return await Storage.GetDatasets<Parcel>(16, 1, geoWikiDatasetGroupId);
         }
 
+        public DataTemplate GetOverviewDataTemplate()
+        {
+            return null;
+        }
+
         public int Save()
         {
             //TODO persist and return ID
             return -1;
         }
-		public event PropertyChangedEventHandler PropertyChanged;
 
-		protected virtual void OnPropertyChanged(string aName)
-		{
-			PropertyChangedEventHandler iHandler = PropertyChanged;
-			if (iHandler != null)
-			{
-				iHandler(this, new PropertyChangedEventArgs(aName));
-			}
-		}
-	}
+        public async void Submit()
+        {
+            await Storage.StoreDatasetAsync(this, -1, 16, 1, geoWikiDatasetGroupId);
+        }
+
+        protected virtual void OnPropertyChanged(string aName)
+        {
+            var iHandler = PropertyChanged;
+            iHandler?.Invoke(this, new PropertyChangedEventArgs(aName));
+        }
     }
 }
