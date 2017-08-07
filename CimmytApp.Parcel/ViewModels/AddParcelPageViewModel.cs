a31bd6849f0f44ded4dadc1c1ed95311fc200067
Mathias Karner <karner@iiasa.ac.at>
2017-08-07 16:41:50 +0200

    WIP

--- CimmytApp.Parcel/ViewModels/AddParcelPageViewModel.cs
+++ CimmytApp.Parcel/ViewModels/AddParcelPageViewModel.cs
@@ -1,37 +1,27 @@
-﻿using CimmytApp.DTO;
-
-namespace CimmytApp.Parcel.ViewModels
+﻿namespace CimmytApp.Parcel.ViewModels
 {
-    using System;
     using System.Collections.Generic;
-    using System.Collections.ObjectModel;
     using System.Linq;
     using System.Windows.Input;
     using Prism.Mvvm;
     using Prism.Navigation;
     using Xamarin.Forms;
 
+    using BusinessContract;
     using DTO.Parcel;
-    using CimmytApp.BusinessContract;
 
     using Helper.Base.DTO;
 
     public class AddParcelPageViewModel : BindableBase, INavigationAware
     {
-        public List<string> AgriculturalCycles { get; } = new List<string> { "Primavera-Verano", "Otoño-Invierno" };
-
-        public List<string> CropTypes { get; } = new List<string> { "Maíz", "Amaranto", "Arroz", "Canola", "Cartamo", "Calabacín", "Garbanzo", "Haba", "Soya", "Ninguno", "Otro", "Cebada", "Frijol", "Trigo", "Triticale", "Sorgo", "Alfalfa", "Avena", "Ajonjolí" };
-
-        private List<string> _years = new List<string> { "2015", "2016", "2017" };
-
-        public List<string> Years
-        {
-            get => _years;
-            set => SetProperty(ref _years, value);
-        }
-
-        public List<string> IrrigationTypes { get; } = new List<string> { "Riego", "Riego de punteo", "Temporal" };
-
+        private readonly List<string> _doubleYears = new List<string>() { "2014-2015", "2015-2016", "2016-2017" };
+        private readonly List<string> _singleYears = new List<string>() { "2015", "2016", "2017" };
+        private ICimmytDbOperations _cimmytDbOperations;
+        private INavigationService _navigationService;
+        private int _pickerAgriculturalCycleSelectedIndex;
+        private int _pickerCropTypesSelectedIndex;
+        private int _pickerIrrigationTypesSelectedIndex;
+        private int _pickerYearsSelectedIndex;
         private bool _tech1Checked;
         private bool _tech2Checked;
         private bool _tech3Checked;
@@ -40,6 +30,73 @@ namespace CimmytApp.Parcel.ViewModels
         private bool _tech6Checked;
         private bool _tech7Checked;
         private bool _tech8Checked;
+        private List<string> _years;
+
+        public AddParcelPageViewModel(INavigationService navigationService, ICimmytDbOperations cimmytDbOperations)
+        {
+            _navigationService = navigationService;
+            _cimmytDbOperations = cimmytDbOperations;
+            Years = new List<string>();
+
+            ClickSave = new Command(SaveParcel);
+            ClickChooseLocation = new Command(ChooseLocation);
+
+            Parcel = new Parcel();
+        }
+
+        public List<string> AgriculturalCycles { get; } = new List<string> { "Primavera-Verano", "Otoño-Invierno" };
+
+        public ICommand ClickChooseLocation { get; set; }
+        public ICommand ClickSave { get; set; }
+        public List<string> CropTypes { get; } = new List<string> { "Maíz", "Amaranto", "Arroz", "Canola", "Cartamo", "Calabacín", "Garbanzo", "Haba", "Soya", "Ninguno", "Otro", "Cebada", "Frijol", "Trigo", "Triticale", "Sorgo", "Alfalfa", "Avena", "Ajonjolí" };
+        public List<string> IrrigationTypes { get; } = new List<string> { "Riego", "Riego de punteo", "Temporal" };
+
+        public Parcel Parcel { get; set; }
+
+        public int PickerAgriculturalCycleSelectedIndex
+        {
+            get => _pickerAgriculturalCycleSelectedIndex;
+            set
+            {
+                _pickerAgriculturalCycleSelectedIndex = value;
+                Parcel.AgriculturalCycle = AgriculturalCycles.ElementAt(value);
+                var yrs = Years;
+                yrs.Clear();
+                yrs.AddRange(value == 0 ? _singleYears : _doubleYears);
+
+                Years = yrs;
+            }
+        }
+
+        public int PickerCropTypesSelectedIndex
+        {
+            get => _pickerCropTypesSelectedIndex;
+            set
+            {
+                _pickerCropTypesSelectedIndex = value;
+                Parcel.Crop = CropTypes.ElementAt(value);
+            }
+        }
+
+        public int PickerIrrigationTypesSelectedIndex
+        {
+            get => _pickerIrrigationTypesSelectedIndex;
+            set
+            {
+                _pickerIrrigationTypesSelectedIndex = value;
+                Parcel.Irrigation = IrrigationTypes.ElementAt(value);
+            }
+        }
+
+        public int PickerYearsSelectedIndex
+        {
+            get => _pickerYearsSelectedIndex;
+            set
+            {
+                _pickerYearsSelectedIndex = value;
+                Parcel.Year = Years.ElementAt(value);
+            }
+        }
 
         public bool Tech1Checked
         {
@@ -121,123 +178,12 @@ namespace CimmytApp.Parcel.ViewModels
             }
         }
 
-        public int PickerYearsSelectedIndex
-        {
-            get => _pickerYearsSelectedIndex;
-            set
-            {
-                _pickerYearsSelectedIndex = value;
-                Parcel.Year = Years.ElementAt(value);
-            }
-        }
-
-        private int _pickerAgriculturalCycleSelectedIndex;
-
-        public int PickerAgriculturalCycleSelectedIndex
-        {
-            get => _pickerAgriculturalCycleSelectedIndex;
-            set
-            {
-                _pickerAgriculturalCycleSelectedIndex = value;
-                Parcel.AgriculturalCycle = AgriculturalCycles.ElementAt(value);
-                switch (value)
-                {
-                    case 0:
-                        Years = _singleYears;
-                        break;
-
-                    case 1:
-                        Years = _doubleYears;
-                        break;
-                }
-            }
-        }
-
-        private int _pickerYearsSelectedIndex;
-
-        private int _pickerCropTypesSelectedIndex;
-
-        public int PickerCropTypesSelectedIndex
-        {
-            get => _pickerCropTypesSelectedIndex;
-            set
-            {
-                _pickerCropTypesSelectedIndex = value;
-                Parcel.Crop = CropTypes.ElementAt(value);
-            }
-        }
-
-        private int _pickerIrrigationTypesSelectedIndex;
-
-        public int PickerIrrigationTypesSelectedIndex
-        {
-            get => _pickerIrrigationTypesSelectedIndex;
-            set
-            {
-                _pickerIrrigationTypesSelectedIndex = value;
-                Parcel.Irrigation = IrrigationTypes.ElementAt(value);
-            }
-        }
-
         public bool Test { get; set; }
 
-        public ICommand ClickChooseLocation { get; set; }
-        public ICommand ClickSave { get; set; }
-
-        public INavigationService _navigationService;
-
-        public Parcel Parcel { get; set; }
-
-        private List<string> _singleYears;
-        private List<string> _doubleYears;
-        private ICimmytDbOperations _cimmytDbOperations;
-
-        public AddParcelPageViewModel(INavigationService navigationService, ICimmytDbOperations cimmytDbOperations)
+        public List<string> Years
         {
-            _navigationService = navigationService;
-            _cimmytDbOperations = cimmytDbOperations;
-
-            ClickSave = new Command(SaveParcel);
-            ClickChooseLocation = new Command(ChooseLocation);
-
-            Parcel = new Parcel();
-            _singleYears = new List<string>() { "2015", "2016", "2017" };
-            _doubleYears = new List<string>() { "2014-2015", "2015-2016", "2016-2017" };
-        }
-
-        private void ChooseLocation()
-        {
-            var parameters = new NavigationParameters { { "GetLocation", true } };
-            _navigationService.NavigateAsync("GenericMap", parameters);
-        }
-
-        private void SaveParcel()
-        {
-            _cimmytDbOperations.AddParcel(Parcel);
-
-            var navigationParameters = new NavigationParameters
-            {
-                { "id", Parcel.ParcelId }
-            };
-            _navigationService.NavigateAsync("MainPage", navigationParameters, true);//This makes issues with Map screen
-        }
-
-        private void AgriculturalCycleChanged()
-        {
-        }
-
-        private void UpdateTechChecked()
-        {
-            var technologies = new List<string>();
-            if (_tech1Checked) technologies.Add("tech1");
-            if (_tech2Checked) technologies.Add("tech2");
-            if (_tech3Checked) technologies.Add("tech3");
-            if (_tech4Checked) technologies.Add("tech4");
-            if (_tech5Checked) technologies.Add("tech5");
-            if (_tech6Checked) technologies.Add("tech6");
-            if (_tech7Checked) technologies.Add("tech7");
-            if (_tech8Checked) technologies.Add("tech8");
-            Parcel.TechnologiesUsed = technologies;
+            get => _years;
+            set => SetProperty(ref _years, value);
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)
@@ -261,6 +207,45 @@ namespace CimmytApp.Parcel.ViewModels
 
         public void OnNavigatingTo(NavigationParameters parameters)
         {
+        }
+
+        private bool CheckFields()
+        {
+            return true;
+        }
+
+        private void ChooseLocation()
+        {
+            var parameters = new NavigationParameters { { "GetLocation", true } };
+            _navigationService.NavigateAsync("GenericMap", parameters);
+        }
+
+        private void SaveParcel()
+        {
+            if (CheckFields() == false)
+            {
+            }
+            _cimmytDbOperations.AddParcel(Parcel);
+
+            var navigationParameters = new NavigationParameters
+            {
+                { "id", Parcel.ParcelId }
+            };
+            _navigationService.NavigateAsync("MainPage", navigationParameters, true);//This makes issues with Map screen
+        }
+
+        private void UpdateTechChecked()
+        {
+            var technologies = new List<string>();
+            if (_tech1Checked) technologies.Add("tech1");
+            if (_tech2Checked) technologies.Add("tech2");
+            if (_tech3Checked) technologies.Add("tech3");
+            if (_tech4Checked) technologies.Add("tech4");
+            if (_tech5Checked) technologies.Add("tech5");
+            if (_tech6Checked) technologies.Add("tech6");
+            if (_tech7Checked) technologies.Add("tech7");
+            if (_tech8Checked) technologies.Add("tech8");
+            Parcel.TechnologiesUsed = technologies;
         }
     }
 }
