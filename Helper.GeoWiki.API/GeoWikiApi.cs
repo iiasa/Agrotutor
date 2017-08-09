d36db1c6b6dd737dfa40be2454151126e53b8c47
Mathias Karner <karner@iiasa.ac.at>
2017-08-09 15:26:01 +0200

    Extracted json based http requests to project

--- Helper.GeoWiki.API/GeoWikiApi.cs
+++ Helper.GeoWiki.API/GeoWikiApi.cs
@@ -1,10 +1,10 @@
 ﻿namespace Helper.GeoWiki.API
 {
     using System.Collections.Generic;
-    using System.Net;
-    using System.Net.Http;
     using System.Threading.Tasks;
     using Newtonsoft.Json;
+
+    using HTTP;
 
     public static class GeoWikiApi
     {
@@ -12,36 +12,15 @@
 
         public static async Task<T> Get<T>(string controller, string action, string param) where T : class
         {
-            T res;
-            using (var httpClient = new HttpClient())
-            {
-                var requestUrl = Url + controller + "/" + action + "/" + param;
-                var response = await httpClient.GetAsync(requestUrl).ConfigureAwait(false);
-                if (response.StatusCode != HttpStatusCode.OK) return null;
-                var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                res = JsonConvert.DeserializeObject<T>(responseContent);
-            }
-            return res;
+            var requestUrl = Url + controller + "/" + action + "/";
+            return await RequestJson.Get<T>(requestUrl, param);
         }
 
         public static async Task<T> Post<T>(string controller, string action, object param) where T : class
         {
-            T res = null;
-            using (var httpClient = new HttpClient())
-            {
-                var parameters = new Dictionary<string, string>() { { "parameter", JsonConvert.SerializeObject(param) } };
-                var encodedContent = new FormUrlEncodedContent(parameters);
-                var requestUrl = Url + controller + "/" + action;
-                var response = await httpClient.PostAsync(requestUrl, encodedContent).ConfigureAwait(false);
-
-                if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    if (string.IsNullOrEmpty(responseContent)) return null;
-                    res = JsonConvert.DeserializeObject<T>(responseContent);
-                }
-            }
-            return res;
+            var requestUrl = Url + controller + "/" + action;
+            var parameters = new Dictionary<string, string>() { { "parameter", JsonConvert.SerializeObject(param) } };
+            return await RequestJson.Post<T>(requestUrl, parameters);
         }
     }
 }
---
+++ Helper.HTTP/RequestJson.cs
@@ -0,0 +1,54 @@
+﻿namespace Helper.HTTP
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public static class RequestJson
+    {
+        public static async Task<T> Get<T>(string url) where T : class
+        {
+            T res;
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url).ConfigureAwait(false);
+                if (response.StatusCode != HttpStatusCode.OK) return null;
+                var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                res = JsonConvert.DeserializeObject<T>(responseContent);
+            }
+            return res;
+        }
+
+        public static async Task<T> Get<T>(string url, string param) where T : class
+        {
+            var fullUrl = (url.EndsWith("/") ? url + param : url + "/" + param);
+            return await Get<T>(fullUrl);
+        }
+
+        public static async Task<T> Post<T>(string url, Dictionary<string, string> param) where T : class
+        {
+            T res = null;
+            using (var httpClient = new HttpClient())
+            {
+                var parameters = new Dictionary<string, string>() { { "parameter", JsonConvert.SerializeObject(param) } };
+                var encodedContent = new FormUrlEncodedContent(parameters);
+                var response = await httpClient.PostAsync(url, encodedContent).ConfigureAwait(false);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    if (string.IsNullOrEmpty(responseContent)) return null;
+                    res = JsonConvert.DeserializeObject<T>(responseContent);
+                }
+            }
+            return res;
+        }
+
+        public static async Task<T> Post<T>(string url) where T : class
+        {
+            return await Post<T>(url, null);
+        }
+    }
+}
