83aede01812d6bc5dd0826a0402bdace103d88de
Mathias Karner <karner@iiasa.ac.at>
2017-10-17 16:06:51 +0200

    Updated addparcelpage

--- CimmytApp.Parcel/ViewModels/AddParcelPageViewModel.cs
+++ CimmytApp.Parcel/ViewModels/AddParcelPageViewModel.cs
@@ -20,34 +20,14 @@
         private readonly ICimmytDbOperations _cimmytDbOperations;
 
         /// <summary>
-        /// Defines the _doubleYears
-        /// </summary>
-        private readonly List<string> _doubleYears = new List<string>() { "2014-2015", "2015-2016", "2016-2017" };
-
-        /// <summary>
         /// Defines the _navigationService
         /// </summary>
         private readonly INavigationService _navigationService;
 
         /// <summary>
-        /// Defines the _singleYears
-        /// </summary>
-        private readonly List<string> _singleYears = new List<string>() { "2015", "2016", "2017" };
-
-        /// <summary>
-        /// Defines the _pickerAgriculturalCycleSelectedIndex
-        /// </summary>
-        private int _pickerAgriculturalCycleSelectedIndex;
-
-        /// <summary>
         /// Defines the _pickerCropTypesSelectedIndex
         /// </summary>
         private int _pickerCropTypesSelectedIndex;
-
-        /// <summary>
-        /// Defines the _pickerIrrigationTypesSelectedIndex
-        /// </summary>
-        private int _pickerIrrigationTypesSelectedIndex;
 
         /// <summary>
         /// Defines the _pickerYearsSelectedIndex
@@ -68,21 +48,6 @@
         /// Defines the _isSaveBtnEnabled
         /// </summary>
         private bool _isSaveBtnEnabled = true;
-
-        /// <summary>
-        /// Defines the _pickerSowingTypesSelectedIndex
-        /// </summary>
-        private int _pickerSowingTypesSelectedIndex;
-
-        /// <summary>
-        /// Defines the _pickerHarvestingTypesSelectedIndex
-        /// </summary>
-        private int _pickerHarvestingTypesSelectedIndex;
-
-        /// <summary>
-        /// Defines the _pickerStorageTypesSelectedIndex
-        /// </summary>
-        private int _pickerStorageTypesSelectedIndex;
 
         /// <summary>
         /// Defines the _pickerMaturityClassesSelectedIndex
@@ -108,10 +73,34 @@
             ClickSave = new DelegateCommand(SaveParcel).ObservesCanExecute(o => IsSaveBtnEnabled);
             ClickChooseLocation = new DelegateCommand(ChooseLocation);
             ClickGetLocation = new DelegateCommand(GetLocation);
+            ClickDelineate = new DelegateCommand(Delineate);
 
             Parcel = new Parcel();
         }
 
+        /// <summary>
+        /// The Delineate
+        /// </summary>
+        private void Delineate()
+        {
+            var parameters = new NavigationParameters
+            {
+                {"Latitude", Parcel.Latitude},
+                {"Longitude", Parcel.Longitude},
+                {"GetPolygon", true},
+                {"parcelId", Parcel.ParcelId }
+            };
+            _navigationService.NavigateAsync("GenericMap", parameters);
+        }
+
+        /// <summary>
+        /// Gets or sets the ClickDelineate
+        /// </summary>
+        public DelegateCommand ClickDelineate { get; set; }
+
+        /// <summary>
+        /// The GetLocation
+        /// </summary>
         private void GetLocation()
         {
             var parameters = new NavigationParameters { { "ChooseLocation", true } };
@@ -121,38 +110,21 @@
         /// <summary>
         /// Gets or sets a value indicating whether IsSaveBtnEnabled
         /// </summary>
-        public bool IsSaveBtnEnabled
-        {
-            get
-            {
-                return _isSaveBtnEnabled;
-            }
-            set
-            {
-                SetProperty(ref _isSaveBtnEnabled, value);
-            }
-        }
+        public bool IsSaveBtnEnabled { get => _isSaveBtnEnabled; set => SetProperty(ref _isSaveBtnEnabled, value); }
 
         /// <summary>
         /// Gets a value indicating whether InformationMissing
         /// </summary>
-        public bool InformationMissing
-        {
-            get { return !IsSaveBtnEnabled; }
-        }
-
-        /// <summary>
-        /// Gets the AgriculturalCycles
-        /// </summary>
-        public List<string> AgriculturalCycles { get; }
-
-= new List<string> { "Primavera-Verano", "Otoño-Invierno" };
+        public bool InformationMissing => !IsSaveBtnEnabled;
 
         /// <summary>
         /// Gets or sets the ClickChooseLocation
         /// </summary>
         public DelegateCommand ClickChooseLocation { get; set; }
 
+        /// <summary>
+        /// Gets or sets the ClickGetLocation
+        /// </summary>
         public DelegateCommand ClickGetLocation { get; set; }
 
         /// <summary>
@@ -163,39 +135,12 @@
         /// <summary>
         /// Gets the CropTypes
         /// </summary>
-        public List<string> CropTypes { get; }
-
-= new List<string> { "Maíz", "Cebada", "Frijol", "Trigo", "Triticale", "Sorgo", "Alfalfa", "Avena", "Ajonjolí", "Amaranto", "Arroz", "Canola", "Cartamo", "Calabacín", "Garbanzo", "Haba", "Soya", "Ninguno", "Otro" };
-
-        /// <summary>
-        /// Gets the IrrigationTypes
-        /// </summary>
-        public List<string> IrrigationTypes { get; }
-
-= new List<string> { "Riego", "Riego de punteo", "Temporal" };
+        public List<string> CropTypes { get; } = new List<string> { "Maíz", "Cebada", "Frijol", "Trigo", "Triticale", "Sorgo", "Alfalfa", "Avena", "Ajonjolí", "Amaranto", "Arroz", "Canola", "Cartamo", "Calabacín", "Garbanzo", "Haba", "Soya", "Ninguno", "Otro" };
 
         /// <summary>
         /// Gets or sets the Parcel
         /// </summary>
         public Parcel Parcel { get; set; }
-
-        /// <summary>
-        /// Gets or sets the PickerAgriculturalCycleSelectedIndex
-        /// </summary>
-        public int PickerAgriculturalCycleSelectedIndex
-        {
-            get => _pickerAgriculturalCycleSelectedIndex;
-            set
-            {
-                _pickerAgriculturalCycleSelectedIndex = value;
-                Parcel.AgriculturalCycle = AgriculturalCycles.ElementAt(value);
-                var yrs = Years;
-                yrs.Clear();
-                yrs.AddRange(value == 0 ? _singleYears : _doubleYears);
-
-                Years = yrs;
-            }
-        }
 
         /// <summary>
         /// Gets or sets the PickerCropTypesSelectedIndex
@@ -212,19 +157,6 @@
         }
 
         /// <summary>
-        /// Gets or sets the PickerIrrigationTypesSelectedIndex
-        /// </summary>
-        public int PickerIrrigationTypesSelectedIndex
-        {
-            get => _pickerIrrigationTypesSelectedIndex;
-            set
-            {
-                _pickerIrrigationTypesSelectedIndex = value;
-                Parcel.Irrigation = IrrigationTypes.ElementAt(value);
-            }
-        }
-
-        /// <summary>
         /// Gets or sets the PickerYearsSelectedIndex
         /// </summary>
         public int PickerYearsSelectedIndex
@@ -236,11 +168,6 @@
                 Parcel.Year = Years.ElementAt(value);
             }
         }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether Test
-        /// </summary>
-        public bool Test { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether UserIsAtParcel
@@ -260,19 +187,35 @@
         {
         }
 
+        /// <inheritdoc />
         /// <summary>
         /// The OnNavigatedTo
         /// </summary>
-        /// <param name="parameters">The <see cref="NavigationParameters"/></param>
+        /// <param name="parameters">The <see cref="T:Prism.Navigation.NavigationParameters" /></param>
         public void OnNavigatedTo(NavigationParameters parameters)
         {
             if (parameters.ContainsKey("GeoPosition"))
             {
-                parameters.TryGetValue("GeoPosition", out object geoPosition);
+                parameters.TryGetValue("GeoPosition", out var geoPosition);
                 if (geoPosition == null) return;
                 var position = (GeoPosition)geoPosition;
                 Parcel.Latitude = position.Latitude;
                 Parcel.Longitude = position.Longitude;
+            }
+
+            if (parameters.ContainsKey("Deliniation"))
+            {
+                parameters.TryGetValue("Deliniation", out var delineation);
+                var polygonObj = new PolygonDto { ListPoints = (List<DTO.GeoPosition>)delineation };
+                if (polygonObj.ListPoints.Count > 0)
+                {
+                    Parcel.Latitude = polygonObj.ListPoints.ElementAt(0).Latitude;
+                    Parcel.Longitude = polygonObj.ListPoints.ElementAt(0).Longitude;
+                }
+                Parcel.SetDeliniation(polygonObj.ListPoints);
+                _cimmytDbOperations.SaveParcelPolygon(Parcel.ParcelId, polygonObj);
+
+                OnPropertyChanged("Parcel");
             }
         }
 
@@ -282,23 +225,6 @@
         /// <param name="parameters">The <see cref="NavigationParameters"/></param>
         public void OnNavigatingTo(NavigationParameters parameters)
         {
-        }
-
-        /// <summary>
-        /// The CheckFields
-        /// </summary>
-        /// <returns>The <see cref="bool"/></returns>
-        private bool CheckFields()
-        {
-            if (Parcel.EstimatedParcelArea == null) return false;
-            if (Parcel.EstimatedParcelArea.Equals(string.Empty)) return false;
-            if (Parcel.ProducerName == null) return false;
-            if (Parcel.ProducerName.Equals(string.Empty)) return false;
-            if (Parcel.ParcelName == null) return false;
-            if (Parcel.ParcelName.Equals(string.Empty)) return false;
-            if (Parcel.Cultivar == null) return false;
-            if (Parcel.Cultivar.Equals(string.Empty)) return false;
-            return true;
         }
 
         /// <summary>
@@ -326,31 +252,14 @@
         }
 
         /// <summary>
-        /// Gets or sets the PickerSowingTypesSelectedIndex
-        /// </summary>
-        public int PickerSowingTypesSelectedIndex
-        {
-            get => _pickerSowingTypesSelectedIndex;
-            set
-            {
-                SetProperty(ref _pickerSowingTypesSelectedIndex, value);
-                Parcel.SowingType = SowingTypes.ElementAt(value);
-            }
-        }
-
-        /// <summary>
         /// Gets the MaturityClasses
         /// </summary>
-        public List<string> MaturityClasses { get; }
-
-            = new List<string> { "Temprana", "Semi-temprana", "Intermedia", "Semi-tardía", "Tardía" };
+        public List<string> MaturityClasses { get; } = new List<string> { "Temprana", "Semi-temprana", "Intermedia", "Semi-tardía", "Tardía" };
 
         /// <summary>
         /// Gets the ClimateTypes
         /// </summary>
-        public List<string> ClimateTypes { get; }
-
-            = new List<string> { "Frío", "Templado/Subtropical", "Tropical", "Híbrido" };
+        public List<string> ClimateTypes { get; } = new List<string> { "Frío", "Templado/Subtropical", "Tropical", "Híbrido" };
 
         /// <summary>
         /// Gets or sets the PickerMaturityClassesSelectedIndex
@@ -377,52 +286,5 @@
                 Parcel.ClimateType = ClimateTypes.ElementAt(value);
             }
         }
-
-        /// <summary>
-        /// Gets the HarvestingTypes
-        /// </summary>
-        public List<string> HarvestingTypes { get; }
-
-            = new List<string> { "Cosecha manual", "Cosecha mecánica" };
-
-        /// <summary>
-        /// Gets the StorageTypes
-        /// </summary>
-        public List<string> StorageTypes { get; }
-
-            = new List<string> { "Almacenamiento poscosecha con tecnologías herméticas", "Almacenamiento poscosecha tradicional" };
-
-        /// <summary>
-        /// Gets or sets the PickerHarvestingTypesSelectedIndex
-        /// </summary>
-        public int PickerHarvestingTypesSelectedIndex
-        {
-            get => _pickerHarvestingTypesSelectedIndex;
-            set
-            {
-                SetProperty(ref _pickerHarvestingTypesSelectedIndex, value);
-                Parcel.HarvestingType = HarvestingTypes.ElementAt(value);
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the PickerStorageTypesSelectedIndex
-        /// </summary>
-        public int PickerStorageTypesSelectedIndex
-        {
-            get => _pickerStorageTypesSelectedIndex;
-            set
-            {
-                SetProperty(ref _pickerStorageTypesSelectedIndex, value);
-                Parcel.StorageType = StorageTypes.ElementAt(value);
-            }
-        }
-
-        /// <summary>
-        /// Gets the SowingTypes
-        /// </summary>
-        public List<string> SowingTypes { get; }
-
-            = new List<string> { "Resiembra", "Siembra" };
     }
 }
