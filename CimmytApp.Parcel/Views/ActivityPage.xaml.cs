77c34116334e4c47ce9d043e5097be11c8bb6856
Mathias Karner <karner@iiasa.ac.at>
2017-11-20 17:32:46 +0100

    Fixes

--- CimmytApp.Parcel/Views/ActivityPage.xaml.cs
+++ CimmytApp.Parcel/Views/ActivityPage.xaml.cs
@@ -1,12 +1,25 @@
-﻿using Xamarin.Forms;
+﻿using CimmytApp.Parcel.ViewModels;
+using Xamarin.Forms;
 
 namespace CimmytApp.Parcel.Views
 {
     public partial class ActivityPage : ContentPage
     {
+        /// <summary>
+        /// Defines the contextObj
+        /// </summary>
+        private readonly ActivityPageViewModel _contextObj;
+
         public ActivityPage()
         {
             InitializeComponent();
+            _contextObj = (ActivityPageViewModel)BindingContext;
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            _contextObj.SaveCommand.Execute();
+            return true;
         }
     }
 }
