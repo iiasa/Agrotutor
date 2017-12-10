3b232032d67028d9f0f737f03e95b555e0f4d44f
Mathias Karner <karner@iiasa.ac.at>
2017-12-10 18:50:38 +0100

    Map update WIP

--- Helper.Map/ViewModels/MapViewModel.cs
+++ Helper.Map/ViewModels/MapViewModel.cs
@@ -1,16 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Helper.Map.Views;
+using Prism.Commands;
+using Prism.Events;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Xamarin.Forms.GoogleMaps;
 
 namespace Helper.Map.ViewModels
 {
     public class MapViewModel : BindableBase, INavigationAware
     {
         private Views.Map _view;
+        private INavigationService _navigationService;
+        private IEventAggregator _eventAggregator;
+        private IPosition _geoLocator;
+        private static string MapCenterParameterName = "MapCenter";
+        private bool _listenForUserLocation;
+        private bool _followUserLocation;
+        private int _maximumLocationAccuracy;
 
-        public MapViewModel()
+        public DelegateCommand UseLocationCommand { get; private set; }
+        public DelineationState CurrentDelineationState { get; private set; }
+        public DelegateCommand<object> MapClickedCommand { get; private set; }
+        public DelegateCommand AcceptDelineationCommand { get; private set; }
+        public DelegateCommand CancelDelineationCommand { get; private set; }
+        public bool ReturnGeolocationButtonEnabled { get; private set; }
+
+        public MapViewModel(IEventAggregator eventAggregator, IPosition geoLocator,
+            INavigationService navigationService)
         {
+            _navigationService = navigationService;
+            _eventAggregator = eventAggregator;
+            _geoLocator = geoLocator;
+
+            UseLocationCommand =
+                new DelegateCommand(UseLocation);//.ObservesCanExecute(o => ReturnGeolocationButtonEnabled);
+            CurrentDelineationState = DelineationState.Inactive;
+            MapClickedCommand = new DelegateCommand<object>(MapClicked);
+            AcceptDelineationCommand = new DelegateCommand(AcceptDelineation);
+            CancelDelineationCommand = new DelegateCommand(CancelDelineation);
+            ReturnGeolocationButtonEnabled = false;
+        }
+
+        private void CancelDelineation()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AcceptDelineation()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapClicked(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UseLocation()
+        {
+            throw new NotImplementedException();
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)
@@ -19,6 +70,79 @@ namespace Helper.Map.ViewModels
 
         public void OnNavigatedTo(NavigationParameters parameters)
         {
+            if (parameters.ContainsKey(GenericMapViewModel.MapTaskParameterName))
+            {
+                parameters.TryGetValue(GenericMapViewModel.MapTaskParameterName, out object mapTask);
+                if (mapTask != null)
+                {
+                    SetMapTask((MapTask)mapTask);
+                }
+            }
+
+            if (parameters.ContainsKey(MapViewModel.MapCenterParameterName))
+            {
+                parameters.TryGetValue(MapViewModel.MapCenterParameterName, out object mapCenter);
+                if (mapCenter != null)
+                {
+                    _view.MoveCamera((CameraUpdate)mapCenter);
+                }
+            }
+            else
+            {
+                _view.MoveCamera(CameraUpdateFactory.NewCameraPosition(new CameraPosition(new Position(-100,20), 15)));
+            }
+
+            if (parameters.ContainsKey(GenericMapViewModel.ListenForUserLocationParameterName))
+            {
+                parameters.TryGetValue(GenericMapViewModel.ListenForUserLocationParameterName,
+                    out object listenForUserLocation);
+                if (listenForUserLocation != null)
+                {
+                    _listenForUserLocation = (bool)listenForUserLocation;
+                }
+            }
+
+            if (parameters.ContainsKey(GenericMapViewModel.FollowUserLocationParameterName))
+            {
+                parameters.TryGetValue(GenericMapViewModel.FollowUserLocationParameterName,
+                    out object followUserLocation);
+                if (followUserLocation != null)
+                {
+                    _followUserLocation = (bool)followUserLocation;
+                }
+            }
+
+            if (parameters.ContainsKey(GenericMapViewModel.MaximumLocationAccuracyParameterName))
+            {
+                parameters.TryGetValue(GenericMapViewModel.MaximumLocationAccuracyParameterName,
+                    out object maximumLocationAccuracy);
+                if (maximumLocationAccuracy != null)
+                {
+                    _maximumLocationAccuracy = (int)maximumLocationAccuracy;
+                }
+            }
+            if (parameters.ContainsKey(GenericMapViewModel.PolygonsParameterName))
+            {
+                parameters.TryGetValue(GenericMapViewModel.PolygonsParameterName, out object polygons);
+                if (polygons != null)
+                {
+                    _view.MapPolygons = (List<Polygon>)polygons;
+                }
+            }
+            if (parameters.ContainsKey(GenericMapViewModel.PointsParameterName))
+            {
+                parameters.TryGetValue(GenericMapViewModel.PointsParameterName, out object points);
+                if (points != null)
+                {
+                    _view.MapPins = (List<Pin>)points;
+                }
+            }
+            _view.MoveCamera(CameraUpdateFactory.NewCameraPosition(new CameraPosition(new Position(48,16),15)));
+        }
+
+        private void SetMapTask(MapTask mapTask)
+        {
+            throw new NotImplementedException();
         }
 
         public void SetViewReference(Views.Map map)
--- Helper.Map/Views/Map.xaml.cs
+++ Helper.Map/Views/Map.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Helper.Map.ViewModels;
 using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
 
 namespace Helper.Map.Views
 {
@@ -10,6 +12,24 @@ namespace Helper.Map.Views
         private MapViewModel _bindingContext;
 
         public List<Pin> MapPins { get; set; }
+        public List<Polygon> MapPolygons { get; set; }
+
+        CameraPosition mapCenter;
+
+        public CameraPosition MapCenter
+        {
+            get
+            {
+                return mapCenter;
+            }
+
+            set
+            {
+                mapCenter = value;
+                map.MoveCamera(CameraUpdateFactory.NewCameraPosition(value));
+            }
+        }
+
 
         public Map()
         {
@@ -18,6 +38,9 @@ namespace Helper.Map.Views
             _bindingContext.SetViewReference(this);
         }
 
-
+        internal void MoveCamera(CameraUpdate mapCenter)
+        {
+            map.MoveCamera(mapCenter);
+        }
     }
 }
