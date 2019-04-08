32bd2e04ee2779a629e2cf601b6d1cb2dce77ee4
Mathias Karner <karner@iiasa.ac.at>
2019-04-08 06:49:07 +0200

    Weather test

--- Agrotutor/Agrotutor/ViewModels/DevPageViewModel.cs
+++ Agrotutor/Agrotutor/ViewModels/DevPageViewModel.cs
@@ -12,5 +12,24 @@ namespace Agrotutor.ViewModels
         {
 
         }
+
+        public DelegateCommand Weather => new DelegateCommand(async () => 
+        {
+            var creds = new UserCredentials{
+                Username = Constants.AWhereWeatherAPIUsername,
+                Password = Constants.AWhereWeatherAPIPassword
+            };
+
+            try
+            {
+                var forecast = await WeatherAPI.GetForecastAsync(20, -100, creds);
+
+                var history = await WeatherAPI.GetObservationsAsync(20, -100, creds, DateTime.Parse("2018-03-01"), DateTime.Parse("2019-04-01"));
+                forecast = await WeatherAPI.GetForecastAsync(20, -100, creds);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        });
     }
 }
