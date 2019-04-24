acbf522862c2683b872de3ee914f8d63baf450b1
Mathias Karner <karner@iiasa.ac.at>
2019-04-25 00:27:50 +0200

    Fixes #116

--- Agrotutor/Agrotutor/Modules/Ciat/Views/PotentialYieldPage.xaml.cs
+++ Agrotutor/Agrotutor/Modules/Ciat/Views/PotentialYieldPage.xaml.cs
@@ -4,9 +4,24 @@ namespace Agrotutor.Modules.Ciat.Views
 {
     public partial class PotentialYieldPage : ContentPage
     {
+
+        private readonly Color ButtonActiveColor = (Color)App.Current.Resources["PrimaryGreen"];
+        private readonly Color ButtonInactiveColor = (Color)App.Current.Resources["PrimaryYellow"];
+
         public PotentialYieldPage()
         {
             InitializeComponent();
+            this.BtnIrrigated.Clicked += (sender, e) =>
+            {
+                this.BtnIrrigated.BackgroundColor = ButtonActiveColor;
+                this.BtnNonIrrigated.BackgroundColor = ButtonInactiveColor;
+            };
+            this.BtnNonIrrigated.Clicked += (sender, e) =>
+            {
+                this.BtnNonIrrigated.BackgroundColor = ButtonActiveColor;
+                this.BtnIrrigated.BackgroundColor = ButtonInactiveColor;
+            };
+            this.BtnNonIrrigated.BackgroundColor = ButtonActiveColor;
         }
     }
 }
