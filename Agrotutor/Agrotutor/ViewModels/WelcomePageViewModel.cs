ac38176bd32b2af0be0ed318b3b428a93de95fb4
Anto Subash <antosubash@live.com>
2019-05-03 12:21:28 +0200

    new welcome page. new desing for simple stat

--- Agrotutor/Agrotutor/ViewModels/WelcomePageViewModel.cs
+++ Agrotutor/Agrotutor/ViewModels/WelcomePageViewModel.cs
@@ -1,29 +1,54 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
 using System.Threading.Tasks;
+using Agrotutor.Core;
 using Microsoft.AppCenter.Crashes;
+using Microsoft.Extensions.Localization;
 using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
+using Prism.Commands;
+using Prism.Navigation;
 using Xamarin.Essentials;
 using XF.Material.Forms.UI.Dialogs;
 
 namespace Agrotutor.ViewModels
 {
-    using System.Windows.Input;
-    using Agrotutor.Core;
-    using Microsoft.Extensions.Localization;
-    using Prism.Commands;
-    using Prism.Navigation;
-
     public class WelcomePageViewModel : ViewModelBase
     {
-        public WelcomePageViewModel(INavigationService navigationService, IStringLocalizer<WelcomePageViewModel> stringLocalizer)
-            : base (navigationService, stringLocalizer)
-        { }
+        public new IStringLocalizer<WelcomePageViewModel> StringLocalizer { get; }
+
+        private ObservableCollection<WelcomeModel> _items;
+
+        public WelcomePageViewModel(INavigationService navigationService,
+            IStringLocalizer<WelcomePageViewModel> stringLocalizer)
+            : base(navigationService, stringLocalizer)
+        {
+            StringLocalizer = stringLocalizer;
+        }
+
+        public ObservableCollection<WelcomeModel> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
 
 
         public DelegateCommand PageAppearingCommand =>
             new DelegateCommand(async () => await PageAppearing());
+
+        public DelegateCommand NavigateToMainPageCommand => new DelegateCommand(
+            async () =>
+            {
+                using (await MaterialDialog.Instance.LoadingDialogAsync(StringLocalizer.GetString("loading")))
+                {
+                    if (Preferences.ContainsKey(Constants.TermsAccepted))
+                        await NavigationService.NavigateAsync("app:///NavigationPage/MapPage");
+                    else
+                        await NavigationService.NavigateAsync("TermsPage");
+                }
+            });
 
         private async Task PageAppearing()
         {
@@ -44,29 +69,36 @@ namespace Agrotutor.ViewModels
                     permissionsToRequest.Add(Permission.Storage);
                 if (permissionsToRequest.Count > 0)
                     await CrossPermissions.Current.RequestPermissionsAsync(permissionsToRequest.ToArray());
+
+                //Items = new ObservableCollection<WelcomeModel> {
+                //    new WelcomeModel {
+                //        Picture = "advice.png",
+                //        Label = _stringLocalizer.GetString("welcome1")
+                //    },
+                //    new WelcomeModel {
+                //        Picture = "advice.png",
+                //        Label = _stringLocalizer.GetString("welcome2")
+
+                //    },
+                //    new WelcomeModel {
+                //        Picture = "advice.png",
+                //        Label = _stringLocalizer.GetString("welcome3")
+
+                //    },
+                //    new WelcomeModel {
+                //        Picture = "advice.png",
+                //        Label = _stringLocalizer.GetString("welcome4")
+
+                //    },
+                //    new WelcomeModel {
+                //        ShowGetStarted = true
+                //    }
+                //};
             }
             catch (Exception e)
             {
                 Crashes.TrackError(e);
             }
         }
-
-        public DelegateCommand NavigateToMainPageCommand => new DelegateCommand(
-            async ()=> {
-
-                using (await MaterialDialog.Instance.LoadingDialogAsync(StringLocalizer.GetString("loading")))
-                {
-                    if (Preferences.ContainsKey(Constants.TermsAccepted))
-                    {
-                        await NavigationService.NavigateAsync("app:///NavigationPage/MapPage");
-                    }
-                    else
-                    {
-                        await NavigationService.NavigateAsync("TermsPage");
-                    }
-                }
-            });
-
-        
     }
 }
